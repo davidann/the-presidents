91036f3974e610f28c1b8e141d63697dc08f5557
David Ann Weiser <davidann@gmail.com>
2013-07-21 20:14:11 -0700

    Generate a scaffold for the project, use rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThePresidents::Application.config.secret_key_base = 'aa6674e24e86b5116ca10faf40e6c6d2f0089e43d28e661c476c30459210d5ee547e26e225a14193d7d2100f767e889becb30979059f9c0460f7c1c6c345429f'
